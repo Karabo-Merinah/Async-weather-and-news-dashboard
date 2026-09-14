df4aeeb7967b400c4c925f77c0fe181d2b7a48fa
Karabo-Merinah <skosanamerinah@gmail.com>
2026-09-14 17:52:08 +0200

    Created a callback version and demonstrated callback hell

---
+++ src/callbackVersion.ts
@@ -0,0 +1,40 @@
+import https from "https"
+
+const WEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude=-26.2041&longitude=28.0473&current=temperature_2m,wind_speed_10m"
+
+const NEWS_DUMMY_URL = "https://dummyjson.com/posts?limit=5"
+
+//Fetching the weather 
+// starts the request
+https.get(WEATHER_URL, (response) => {
+    //response is recieved in form of chunks  which is binary so we convert it to string 
+    let body = ""
+    response.on("data", (chunk) => {
+        body += chunk
+    })
+    response.on("end", () => {
+        //body holds the full string and parse it then grab current  from weather structure
+        const weather = JSON.parse(body).current
+
+        
+        //fetching the news using the url 
+        https.get(NEWS_DUMMY_URL, (response2) => {
+            let desc = ""
+            response2.on("data", (chunk) => {
+                desc += chunk
+            })
+            //desc is converted to string as it is chunks and parse it to json to retrieve only posts structure 
+            response2.on("end", () => {
+                const news = JSON.parse(desc).posts
+                console.log("=====News and Weather Dashboard =====")
+                console.log(`Weather: ${weather.temperature_2m}°C`)
+                console.log("News:")
+                for(let i=0;i<news.length;i++){
+                    const post=news[i]
+                    console.log(post.title)
+                }
+            })
+        })
+    })
+
+})
